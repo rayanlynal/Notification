8fea6509c6457d6368b166695da66b4ea7ae036c
Rayan Mascarenhas <rayanm47@gmail.com>
2020-09-27 22:57:49 -0400

    database config

--- notification/src/main/java/com/amazonsns/controller/NotificationController.java
+++ notification/src/main/java/com/amazonsns/controller/NotificationController.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
@@ -29,4 +31,9 @@ public class NotificationController {
         return "Greetings from Spring Boot!";
     }
 
+    /*@PostMapping("/newuser")
+    public String addNewUser(@RequestBody User user) {
+
+
+    }*/
 }
